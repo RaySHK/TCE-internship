fb56a11ddeb19d5ada24a979b87b84665ef4f657
Sheik Rayyan Ahmed <99989484+RaySHK@users.noreply.github.com>
2023-11-02 08:25:53 +0530

    Delete final project/backend directory

--- final project/backend/src/validation/valid.js
+++
@@ -1,9 +0,0 @@
-const validEmail = (email) =>{
-
-    return email.match( /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/ )
-}
-
-const validPass = (password) =>{
-    return password.match(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8, 20}$/)
-}
-module.exports = {validEmail,validPass}
